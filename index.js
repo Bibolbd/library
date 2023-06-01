1a99bae5930484e54b5e725a2fe1ede04774e048
bibolbd <bilguundalai3355@gmail.com>
2023-06-01 13:14:32 +0800

    feat: display library func

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-let myLibrary = []
+let myLibrary = [];
 let bookNumber = 0;
-const addBook = document.getElementById('addBook')
+const addBook = document.getElementById("addBook");
 
 function Book(title, author, pages, isRead) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
-    this.info  = function() {
-        return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.isRead
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.info = function () {
+    return (
+      this.title +
+      " by " +
+      this.author +
+      ", " +
+      this.pages +
+      " pages, " +
+      this.isRead
+    );
+  };
 }
 
-addBook.addEventListener('click', function (Event) {
-    const title = document.getElementById('title').value
-    const author = document.getElementById('author').value
-    const numberOfPages = document.getElementById('numberOfPages').value
-    const isRead = document.getElementById('isRead').checked ? 'finished' : "not yet read"
-    myLibrary.push(new Book(title, author, numberOfPages, isRead))
-    Event.preventDefault()
-    console.log(myLibrary);
-})
+addBook.addEventListener("submit", function (Event) {
+  Event.preventDefault();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const numberOfPages = document.getElementById("numberOfPages").value;
+  const isRead = document.getElementById("isRead").checked
+    ? "finished"
+    : "not yet read";
+  myLibrary.push(new Book(title, author, numberOfPages, isRead));
+  // console.log(myLibrary)
+  displayLibrary();
+});
+
+function displayLibrary() {
+  let library = myLibrary.map(function (value, index) {
+    return {
+      key: index,
+      title: value.title,
+      author: value.author,
+      pages: value.pages,
+      isRead: value.isRead,
+    };
+  });
+  console.log(library);
+}
